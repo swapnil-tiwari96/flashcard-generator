70285a28827637426f1b6d41c932db8587d22620
avinash-s91 <113238956+avinash-s91@users.noreply.github.com>
2022-09-12 15:42:49 +0530

    tab formation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import './App.css';
 
-function App()
-{
-  return (
-    <div className="App">
-      Hellow World
-    </div>
-  );
+import "./App.css";
+import React, { Component } from "react";
+import Tabview from "./components/TabView/Tabview";
+import MyFlashcard from "./components/MyFlashcard";
+import CreateNew from'./components/CreateNew';
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="center">
+          <Tabview
+            title={"Tab Test"}
+            tabs={[
+              { name: "Create New", content: CreateNew },
+              { name: "My Flashcard", content: MyFlashcard },
+              // { name: "tab 3", content: tabname },
+              // { name: "Tab 4", content: tabname },
+            ]}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
+import './App.css';
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <App />
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
 );
-
